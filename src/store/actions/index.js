6248d788c6feda24ac49c1b6de30f27fc9eff27f
vadiminsk <vadiminsk90@gmail.com>
2021-05-20 00:26:44 +0300

    add spinner and load posts by click on the button

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -2,7 +2,7 @@ import * as api from '../../api';
 
 import { GET_POSTS } from '../types';
 
-export const getPosts = () => ({
+export const getPosts = (homePost, page, order, limit) => ({
   type: GET_POSTS,
-  payload: api.getPosts(),
+  payload: api.getPosts(homePost, page, order, limit),
 });
